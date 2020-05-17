962b35e50e786b466f22daaaf04775bb1cecbb76
lingxuan588 <1096121491@qq.com>
2020-05-17 21:40:32 +0800

    主页模块-头像退出系统完成

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -15,17 +15,18 @@
 <el-col  class="right"  :span="12" >
 <!-- 再次放置一个row组件 -->
 <el-row type="flex" justify="end" align="middle" >
-    <img src="http://img3.imgtn.bdimg.com/it/u=3683328035,1562039468&fm=11&gp=0.jpg" alt="">
-<!-- 下拉菜单 -->
-<el-dropdown  trigger="click">
+    <!-- <img :src="userInfo.photo"> -->
+    <img src="http://img1.imgtn.bdimg.com/it/u=3986366078,1066070541&fm=11&gp=0.jpg" alt="" srcset="">
+<!-- 下拉菜单 点击菜单会触发command事件-->
+<el-dropdown  trigger="click" @command="clickMenu">
 <!-- 显示的内容 -->
-<span>是小喵呀</span>
+<span>是你</span>
 <!-- 下拉内容需要做具名插槽dropdown  -->
 <el-dropdown-menu  slot="dropdown" >
     <!-- 下拉选项el-dropdown-item作为下拉选项组件 -->
-<el-dropdown-item>个人信息</el-dropdown-item>
-<el-dropdown-item>git地址</el-dropdown-item>
-<el-dropdown-item>退出</el-dropdown-item>
+<el-dropdown-item command="info">个人信息</el-dropdown-item>
+<el-dropdown-item command="git">git地址</el-dropdown-item>
+<el-dropdown-item command="lgout">退出</el-dropdown-item>
 </el-dropdown-menu>
 </el-dropdown>
 </el-row>
@@ -35,7 +36,40 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      userInfo: {}// 用户个人信息
+    }
+  },
+  methods: {
+    clickMenu (command) {
+      // 需要处理三种情况
+      if (command === 'info') {
+        // 点击了个人信息
+      } else if (command === 'git') {
+      // 如果点击了git苍鹭就跳转到git仓库
+        window.location.href = 'https://github.com/yaoshengming/94-toutiaopc'
+      } else {
+        // 退出系统1.删除token 2.跳转登录页
+        window.localStorage.removeItem('user-token')// 删除localstorage中某个选项
+        this.$router.push('/login')// 跳回登录页  编程式导航
+      }
+    }
+  },
+  created () {
+    const token = localStorage.getItem('user-token')// 从兜里拿钥匙 也就是从缓存中取token
+    // 获取用户个人信息
+    this.$axios({
+      url: '/user/profile', // 请求地址
+      headers: {
+        Authorization: `Bearer ${token}`// 格式要求Bearer+token
+      }// 请求头参数
+    }).then(result => {
+      // 如果加载成功我们要将数据赋值给userInfo
+      // this.userInfo = result.data.data
+      debugger
+    })
+  }
 }
 </script>
 
