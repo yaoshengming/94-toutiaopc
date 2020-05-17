705719e27a8a601311b39fe4ea9a36a9c464bd67
lingxuan588 <1096121491@qq.com>
2020-05-18 01:32:15 +0800

    面包屑组件封装完成

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@
 // Vue.use会调用对象中的install方法 install方法第一个参数是vue对象
 import LayoutHeader from './home/layout-header'
 import LayoutAside from './home/layout-aside'
+import BreadCrumb from './common/bread'
 export default {
   install: function (Vue) {
     // 注册全局组件 Vue
     Vue.component('layout-header', LayoutHeader)// 注册头部组件
     Vue.component('layout-aside', LayoutAside)// 注册左侧导航组件
+    Vue.component('bread', BreadCrumb)// 注册一个面包屑组件
   }
 }
